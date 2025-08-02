c75fdb01f98b5488ded2084280be650a5bb7e0c8
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-08-02 19:52:34 +0545

    Add tags validation in validateBlog

--- middleware/validateBlog.js
+++ middleware/validateBlog.js
@@ -1,5 +1,7 @@
+import mongoose from 'mongoose'
+
 const validateBlog = (req, res, next) => {
-	const { title, description } = req.body
+	const { title, description, tags } = req.body
 
 	if (typeof title !== 'string' || typeof description !== 'string') {
 		return res.status(400).json({
@@ -19,6 +21,23 @@ const validateBlog = (req, res, next) => {
 		return res.status(400).json({
 			message: 'Description must be between 100 and 10,000 characters long.'
 		})
+	}
+
+	if (tags) {
+		if (!Array.isArray(tags)) {
+			return res.status(400).json({ 
+				message: 'Tags must be an array' 
+			})
+		}
+
+		const invalid = tags.find(
+			tagId => !mongoose.Types.ObjectId.isValid(tagId)
+		)
+		if (invalid) {
+			return res.status(400).json({
+				message: `${invalid} is not a valid tag ID. Please check your input.`
+			})
+		}
 	}
 
 	next()
